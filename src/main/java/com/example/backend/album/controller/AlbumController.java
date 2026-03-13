73b29925fd072d5bcfc48515549b198034680c7a
ruby747 <jun2002usa@gmail.com>
2026-03-13 11:55:36 +0900

    수정

--- src/main/java/com/example/backend/album/controller/AlbumController.java
+++ src/main/java/com/example/backend/album/controller/AlbumController.java
@@ -173,10 +173,7 @@ public class AlbumController {
             @PathVariable Long albumId,
             @RequestBody AlbumUpdateRequests  body,
             Authentication authentication) {
-    	
-    	 System.out.println("photoIds type: " + body.getPhotoIds().getClass().getName());
-    	    System.out.println("tags type: " + body.getTags().getClass().getName());
-        try 
+        try {
 
             String title = body.getTitle();
             String bodyText = body.getBodyText();
